b34b8f9e3c2c851aba25ea7beceee117ae64edda
Jamal Waddell <jamalwaddell81@gmail.com>
2024-11-13 13:25:23 -0400

    Initial commit

---
+++ src/main/java/com/mycompany/taxiservice/TaxiService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taxiservice;
+
+/**
+ *
+ * @author jamwe
+ */
+public class TaxiService {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
